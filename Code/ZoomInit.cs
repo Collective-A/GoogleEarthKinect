618929233a8f94f43fc665b830366e5ad3ca5693
Tyler <tsg7741@gmail.com>
2012-10-16 17:54:29 -0400

    Initial Zoom stance shit
    Init stance for beinging to zoom in or out

---
+++ Code/ZoomInit.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Microsoft.Kinect;
+
+namespace Kinect.GoogleEarth
+{
+    public ZoomInit : BaseGesture
+	{
+        GestureResult CheckGesture(Skeleton skeleton)
+        {
+            if (skeleton == null)
+            {
+                return GesturePartResult.Fail;
+            }
+            
+            if (skeleton.Joints[JointType.HandLeft].Position.X = skeleton.Joints[JointType.ElbowLeft].Position.X)
+                   && (skeleton.Joints[JointType.HandRight].Position.X = skeleton.Joints[JointType.ElbowRight].Position.X)
+            {
+                return GesturePartResult.Succeed;
+            }
+
+            // hands dropped
+            return GesturePartResult.Fail;
+        }
+    }
+	}
+}
