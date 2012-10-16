37392952ee00a94dc66550dc562414cc59845a7e
Tyler <tsg7741@gmail.com>
2012-10-16 17:17:17 -0400

    Added some shit
    GestureType & BasicGesture

---
+++ Code/BaseGesture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Kinect;
+
+namespace Kinect.GoogleEarth
+{
+	public interface BaseGesture
+	{
+        
+	}
+}
---
+++ Code/GestureType.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Kinect.GoogleEarth
+{
+	public enum GestureResult
+	{
+        // Gesture fail
+        Fail,
+
+        // Gesture suceed
+        Succeed,
+
+        // Gesture undetermined
+        Pondering 
+	}
+
+    public enum GestureType
+    {
+
+
+
+
+    }
+}
