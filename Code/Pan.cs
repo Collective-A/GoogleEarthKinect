4378e222229eb74fd00ee64369a95629cc32e43e
Tyler <tsg7741@gmail.com>
2012-10-17 16:18:50 -0400

    Created Pan
    Pan up, down, left, right

---
+++ Code/Pan.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Kinect;
+
+namespace Kinect.GoogleEarth
+{
+    class PanUp : BaseGesture
+    {
+        public GestureResult CheckGesture(Skeleton skeleton)
+        {
+            if (skeleton == null)
+            {
+                return GestureResult.Fail;
+            }
+
+            if (skeleton.Joints[JointType.HandRight].Position.Y > skeleton.Joints[JointType.ShoulderRight].Position.Y &&
+                skeleton.Joints[JointType.HandLeft].Position.Y < skeleton.Joints[JointType.WaistLeft].Position.Y)
+            {
+                return GestureResult.Succeed;
+            }
+
+            // hands dropped
+            return GestureResult.Fail;
+        }
+    }
+
+    class PanDown : BaseGesture
+    {
+        public GestureResult CheckGesture(Skeleton skeleton)
+        {
+            if (skeleton == null)
+            {
+                return GestureResult.Fail;
+            }
+
+            if (skeleton.Joints[JointType.HandRight].Position.Y < skeleton.Joints[JointType.ShoulderRight].Position.Y &&
+                skeleton.Joints[JointType.HandLeft].Position.Y < skeleton.Joints[JointType.HipLeft].Position.Y && 
+                skeleton.Joints[JointType.HandRight].Position.Y < skeleton.Joints[JointType.ElbowRight].Position.Y)
+            {
+                return GestureResult.Succeed;
+            }
+
+            // hands dropped
+            return GestureResult.Fail;
+        }
+    }
+
+    class PanRight : BaseGesture
+    {
+        public GestureResult CheckGesture(Skeleton skeleton)
+        {
+            if (skeleton == null)
+            {
+                return GestureResult.Fail;
+            }
+
+            if (skeleton.Joints[JointType.HandRight].Position.X > skeleton.Joints[JointType.ShoulderRight].Position.X &&
+                skeleton.Joints[JointType.HandRight].Position.Y > skeleton.Joints[JointType.HipRight].Position.Y &&
+                skeleton.Joints[JointType.HandLeft].Position.Y < skeleton.Joints[JointType.HipLeft].Position.Y) 
+            {
+                return GestureResult.Succeed;
+            }
+
+            // hands dropped
+            return GestureResult.Fail;
+        }
+    }
+
+    class PanLeft : BaseGesture
+    {
+        public GestureResult CheckGesture(Skeleton skeleton)
+        {
+            if (skeleton == null)
+            {
+                return GestureResult.Fail;
+            }
+
+            if (skeleton.Joints[JointType.HandRight].Position.X < skeleton.Joints[JointType.ShoulderRight].Position.X &&
+                skeleton.Joints[JointType.HandRight].Position.Y > skeleton.Joints[JointType.HipRight].Position.Y &&
+                skeleton.Joints[JointType.HandLeft].Position.Y < skeleton.Joints[JointType.HipLeft].Position.Y)
+            {
+                return GestureResult.Succeed;
+            }
+
+            // hands dropped
+            return GestureResult.Fail;
+        }
+    }
+}
